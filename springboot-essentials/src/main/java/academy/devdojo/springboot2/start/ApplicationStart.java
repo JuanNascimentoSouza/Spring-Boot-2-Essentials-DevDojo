da0dda17fe3032c5de23d986f43f3fac1d774ef6
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-12-17 23:23:05 -0300

    date util class, spring boot application

--- springboot-essentials/src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
+++ springboot-essentials/src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
@@ -2,10 +2,11 @@ package academy.devdojo.springboot2.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "academy.devdojo.springboot2")
+@SpringBootApplication
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class,args);
