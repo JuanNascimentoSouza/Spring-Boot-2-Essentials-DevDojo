bb1cd9aa33a3f855842077c4299254870e49550f
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-12-19 19:25:06 -0300

    service,repository,application properties

--- springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
+++ springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/controller/AnimeController.java
@@ -1,10 +1,14 @@
 package academy.devdojo.springbootessentials.controller;
 
 import academy.devdojo.springbootessentials.domain.Anime;
+import academy.devdojo.springbootessentials.services.AnimeService;
 import academy.devdojo.springbootessentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +16,23 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
     //localhost:8080/anime/list
-    @GetMapping(path = "list")
-    public List<Anime> list(){
+    @GetMapping
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
