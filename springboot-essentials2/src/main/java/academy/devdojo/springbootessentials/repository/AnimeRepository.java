3efbdee408d97dedb12119c5253908750ab9c406
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-12-23 23:32:55 -0300

    dto

--- springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/repository/AnimeRepository.java
+++ springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/repository/AnimeRepository.java
@@ -1,10 +1,11 @@
 package academy.devdojo.springbootessentials.repository;
 
 import academy.devdojo.springbootessentials.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnimeRepository {
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
     List<Anime> listAll();
 
 }
