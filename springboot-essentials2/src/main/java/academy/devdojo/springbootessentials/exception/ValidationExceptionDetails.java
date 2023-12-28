092fd5e151568b347f0a7940ca2307ae51ec1d33
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-12-28 20:40:21 -0300

    validacao, exception details

--- springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/exception/ValidationExceptionDetails.java
+++ springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/exception/ValidationExceptionDetails.java
@@ -3,9 +3,9 @@ package academy.devdojo.springbootessentials.exception;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
-
 @Getter
 @SuperBuilder
-public class BadRequestExceptionDetails extends ExceptionDetails {
-
+public class ValidationExceptionDetails extends ExceptionDetails {
+    private final String fields;
+    private final String fieldsMessage;
 }
