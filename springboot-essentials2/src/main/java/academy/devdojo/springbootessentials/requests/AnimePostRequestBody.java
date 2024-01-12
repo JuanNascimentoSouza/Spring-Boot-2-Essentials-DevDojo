8d1df0bc7066c5c7468ee41acf1c93863357abc6
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2024-01-12 15:31:29 -0300

    anime post request body

--- springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/requests/AnimePostRequestBody.java
+++ springboot-essentials2/src/main/java/academy/devdojo/springbootessentials/requests/AnimePostRequestBody.java
@@ -1,14 +1,25 @@
 package academy.devdojo.springbootessentials.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
+
     @NotEmpty(message = "The anime name cannot be empty")
+    @Schema(description = "This is the anime's name", example = "Dragon Ball Z" , required = true)
     private String name;
+
+    @Schema(description = "É um código de barras no padrão EAN definido pela GS1", example = "12345678910" , required = true)
+    private String ean13;
+
+    private String url;
 }
